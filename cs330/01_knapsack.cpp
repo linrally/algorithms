f419e1141db0c1a198e4748406a0ac6c22a3f309
Rally Lin <linrally@gmail.com>
2025-12-19 14:11:08 -0800

    knapsack 2 row + reconstruction

--- cs330/01_knapsack.cpp
+++ cs330/01_knapsack.cpp
@@ -17,7 +17,37 @@ int knapsack(const vector<Item> &items, const int B){
 			dp[i][j] = max(exclude, include);
 		}
 	}
+	
+	// reconstruct
+	// int i = n, j = B;
+	// vector<Item> selected;
+	// while(i > 0){
+	// 	if(dp[i][j] == dp[i-1][j-items[i-1].weight] + items[i-1].price){
+	// 		selected.push_back(items[i-1]);
+	// 		j -= items[i-1].weight;
+	// 	}
+	// 	i--;
+	// }
+	// for(Item &it: selected) cout << it.price << " " << it.weight << endl;
+
 	return dp[n][B];
+}
+
+int knapsack_2row(const vector<Item> &items, const int B){
+	int n = items.size();
+	vector<int> prev(B + 1);
+	vector<int> cur(B + 1);
+	for(int i = 1; i <= n; i++){
+		for(int j = 0; j<= B; j++){
+			if(j >= items[i-1].weight){
+				cur[j] = max(prev[j], prev[j - items[i-1].weight] + items[i-1].price);
+			} else {
+				cur[j] = prev[j];
+			}
+		}
+		prev = cur;
+	}
+	return cur[B];
 }
 
 int main() {
@@ -28,5 +58,6 @@ int main() {
 	}
 	int B; cin >> B;
 	cout << knapsack(items, B) << endl;
+	// cout << knapsack_2row(items, B) << endl;
 	return 0;
 }
