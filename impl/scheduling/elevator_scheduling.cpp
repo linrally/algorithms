ff39411d9ebf1ff6bb07d6be6505ef92ebfd0f6e
Rally Lin <linrally@gmail.com>
2026-01-08 21:50:26 -0500

    elevator scheduling

--- impl/scheduling/elevator_scheduling.cpp
+++ impl/scheduling/elevator_scheduling.cpp
@@ -1,48 +1,110 @@
 #include <iostream>
 using namespace std;
 
-// Elevator scheduling is also used for disk scheduling
-// Want to minimize the total seek time (time to move disk arm to the track where data is located)
-
-struct Request{
-	int source {};
-	int dest {};
-};
-
 using Floor = int;
 
-vector<Floor> LOOKPickup(vector<Request> requests, Floor cur, int direction){ // pickup order only
-	vector<Floor> left, right, pickup;
-	for(Request r: requests){
-		if (r.source < cur) left.push_back(r.source);
-		else right.push_back(r.source);
+// First Come First Serve (FCFS)
+vector<Floor> FCFS(vector<Floor> requests, Floor cur) {
+	return requests;
+}
+
+// Shortest Seek Time First (SSTF)
+// More optimal for total time, but may result in starvation
+vector<Floor> SSTF(vector<Floor> requests, Floor cur){
+	vector<Floor> stops;
+	vector<Floor> remaining = requests;
+	while(remaining.size() > 0){
+		int minDist = abs(cur - remaining[0]);
+		int minIdx = 0;
+		for(int i = 1; i < remaining.size(); i++){
+			if(abs(cur - remaining[i]) < minDist){
+				minIdx = i;
+				minDist = abs(cur - remaining[i]);
+			}
+		}
+		stops.push_back(remaining[minIdx]);
+		cur = remaining[minIdx];
+		remaining.erase(remaining.begin() + minIdx);
 	}
+	return stops;
+}
 
-	sort(left.begin(), left.end(), greater<Floor>()); 
-	sort(right.begin(), right.end());
+// SCAN. Move in one direction, servicing all requests until reaching the end, then reverse.
+// https://en.wikipedia.org/wiki/Elevator_algorithm
+const int MAX_FLOOR = 100;
+vector<Floor> SCAN(vector<Floor> requests, Floor cur){ // initial direction technically matters here, but omitted for simplicity
+  vector<Floor> path;
+  vector<Floor> up, down;
 
-	if(direction == 1){ // up
-		for (Floor f : right)
-			pickup.push_back(f);
-		for (Floor f : left)
-			pickup.push_back(f);
-	} else { // down
-		for (Floor f : left)
-			pickup.push_back(f);
-		for (Floor f : right)
-			pickup.push_back(f);
-	}
+  path.push_back(cur);
 
-	return pickup;
+  for (Floor f : requests) {
+    if (f >= cur) up.push_back(f);
+    else down.push_back(f);
+  }
+
+  sort(up.begin(), up.end());
+  sort(down.begin(), down.end(), greater<Floor>());
+
+  for (Floor f : up) {
+    if (path.back() != f)
+      path.push_back(f);
+    cur = f;
+  }
+
+  if (cur != MAX_FLOOR) { // go to top
+    cur = MAX_FLOOR;
+    path.push_back(cur);
+  }
+
+  for (Floor f : down) {
+    if (path.back() != f)
+      path.push_back(f);
+    cur = f;
+  }
+
+  return path;
+}
+
+
+// LOOK. Instead of going all the way to the end, reverse direction at the last request in each direction.
+// https://en.wikipedia.org/wiki/LOOK_algorithm
+vector<Floor> LOOK(vector<Floor> requests, Floor cur){
+  vector<Floor> path;
+  vector<Floor> up, down;
+
+  path.push_back(cur);
+
+  for (Floor f : requests) {
+    if (f >= cur) up.push_back(f);
+    else down.push_back(f);
+  }
+
+  sort(up.begin(), up.end());
+  sort(down.begin(), down.end(), greater<Floor>());
+
+  for (Floor f : up) {
+    if (path.back() != f)
+      path.push_back(f);
+    cur = f;
+  }
+
+  for (Floor f : down) {
+    if (path.back() != f)
+      path.push_back(f);
+    cur = f;
+  }
+
+  return path;
+
 }
 
 int main() {
 	int N; cin >> N;
-	vector<Request> requests(N);
-	for(int i = 0; i < N; i++) cin >> requests[i].source >> requests[i].dest;
+	vector<Floor> requests(N);
+	for(int i = 0; i < N; i++) cin >> requests[i];
 	Floor cur; cin >> cur;
-	int direction; cin >> direction;
-	vector<Floor> res = LOOKPickup(requests, cur, direction);
+	vector<Floor> res = LOOK(requests, cur);
 	for(Floor f: res) cout << f << " ";
 	cout << endl;
 	return 0;
